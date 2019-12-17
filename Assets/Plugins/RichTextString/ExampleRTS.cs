d1798b7ed297e9c1bfd359d1bab04215ca1fb3d3
Bouiss <yann.bouissou@gmail.com>
2019-12-17 07:25:51 +0100

    Add exemple scene with simple UI, Add RTS Script, Add Example Script. Suppr SampleScene

---
+++ Assets/Plugins/RichTextString/ExampleRTS.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static RTS;
+
+public class ExampleRTS : MonoBehaviour
+{
+
+    // this is only for test the RTString ! 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(RTString("The Sky is Blue !!!! ", Color.blue, true, false));
+            Debug.Log(RTString("The Sky is Blue ! ", Color.blue, true, false)+" "+ RTS.RTString("And Love Is Pink !!!! ", "#E43CFF", false, true));
+        }
+    }
+
+}
---
+++ Assets/Plugins/RichTextString/RTS.cs
@@ -0,0 +1,65 @@
+﻿
+//************* LICENCE *************
+// Made with love by Yann Bouissou.
+//
+// Free for commercial an personnal use. '(O.<)'
+// Kiss !
+
+using System.Text;
+using UnityEngine;
+
+static class RTS
+{
+    /**********************************/
+    /*   RTS ==> "Rich Text String"   */
+    /**********************************/
+
+    public static string RTString(string text, Color textColor,bool isBold = false, bool isItalic = false )
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendFormat("<color=#{0}>{1}</color>", ColorUtility.ToHtmlStringRGBA(textColor), text);
+        
+        if (isBold)
+        {
+            sb.Insert(0, "<b>");
+            sb.Append("</b>");
+        }
+
+        if (isItalic)
+        {
+            sb.Insert(0, "<i>");
+            sb.Append("</i>");
+        }
+
+
+        return sb.ToString();
+
+    }
+    public static string RTString(string text, string hexColorWithSharp, bool isBold = false, bool isItalic = false)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendFormat("<color={0}>{1}</color>", hexColorWithSharp, text);
+
+        if (isBold)
+        {
+            sb.Insert(0, "<b>");
+            sb.Append("</b>");
+        }
+
+        if (isItalic)
+        {
+            sb.Insert(0, "<i>");
+            sb.Append("</i>");
+        }
+
+
+        return sb.ToString();
+    }
+
+    //  Exemple of usage : 
+
+    // Add : using static RTS;
+
+    //  Debug.Log(RTString("The Sky is Blue !!!! ", Color.blue, true, false));
+    //  Debug.Log(RTString("The Sky is Blue ! ", Color.blue, true, false)+" "+ RTS.RTString("And Love Is Pink !!!! ", "#E43CFF", false, true));
+}
