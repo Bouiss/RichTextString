ccaa6c1a4c6ab1986e5d718aada0dde61a41b55a
Bouiss <yann.bouissou@gmail.com>
2019-12-27 16:25:14 +0100

    Add thx to Remy in the script ^^ Add doc to RTS.cs Add a size constraint in Size style Delete of the old RTS method with bools. Add info text to exemples

--- Assets/Plugins/RichTextString/Example/ExampleRTS.cs
+++ Assets/Plugins/RichTextString/Example/ExampleRTS.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ExampleRTS : MonoBehaviour
 {
-
-    // this is only for test the RTString ! 
+   
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(RTS.String("The Sky is Blue !!!! ", RTS.Color(Color.blue), RTS.Bold()));
+            // One string with blue and bold text
+            Debug.Log(RTS.String("The Sky is Blue !!!! ", RTS.Color(Color.green), RTS.Bold()));
+
+            // Two strings with differents params
             Debug.Log(string.Format("{0} {1}",
                 RTS.String("The Sky is Blue ! ", RTS.Color(Color.blue), RTS.Bold()),
                 RTS.String("And Love Is Pink !!!! ", RTS.Size(20), RTS.Color("E43CFF"), RTS.Italic())
                 ));
+
+            // one string with with an Oversizer text (it will be reduce to 35 automaticly)
+            Debug.Log(RTS.String("test", RTS.Italic(), RTS.Color(Color.red),RTS.Size(65)));
         }
     }
-
 }
--- Assets/Plugins/RichTextString/RTS.cs
+++ Assets/Plugins/RichTextString/RTS.cs
@@ -1,6 +1,7 @@
 ﻿
 //************* LICENCE *************
 // Made with love by Yann Bouissou.
+// Thx to Remy Maetz for the update.
 //
 // Free for commercial an personnal use. '(O.<)'
 // Kiss !
@@ -8,8 +9,15 @@
 using System.Text;
 using UnityEngine;
 
+/// <summary>
+/// RTS ==> "Rich Text String"   
+/// <para>Add style to your text field with string params</para>
+/// </summary>
 class RTS
 {
+    //****** Styles *******//
+    #region Styles
+
     public class Style
     {
         public class S_Bold : Style { }
@@ -23,33 +31,81 @@ class RTS
             public string value;
         }
     }
-
+    /// <summary>
+    /// Bold text (no parameters required)
+    /// </summary>
+    /// <para>example: RTS.Bold()</para>
+    /// <returns></returns>
     public static Style.S_Bold Bold()
     {
         return new Style.S_Bold();
     }
+    /// <summary>
+    /// Italic text to String (no parameters required)
+    /// </summary>
+    /// <para>example: RTS.Italic()</para>
+    /// <returns></returns>
     public static Style.S_Italic Italic()
     {
         return new Style.S_Italic();
     }
+
+    static int MaxFontSize = 35;
+    static int MinFontSize = 10;
+    /// <summary>
+    /// Define the size of the test
+    /// </summary>
+    /// <param name="size">define the size of the text, it's automatically constrained (10-25)
+    /// <para>example: RTS.Size(12)</para>
+    /// </param>
+    /// <returns></returns>
     public static Style.S_Size Size(int size)
     {
+        if (size>20)
+        {
+            size= MaxFontSize; 
+        }
+        if (size<10)
+        {
+            size= MinFontSize;
+        }
         return new Style.S_Size() { value = size };
     }
-
+    /// <summary>
+    /// Define the text color with hexadecimal value
+    /// </summary>
+    /// <param name="hexColor">Use hexadecimal value in string without #
+    /// <para>example: RTS.Color("E43CFF")</para>
+    /// </param>
+    /// <returns></returns>
     public static Style.S_Color Color(string hexColor)
     {
         return new Style.S_Color() { value = hexColor };
     }
-
+    /// <summary>
+    /// Define the text color with Color value
+    /// </summary>
+    /// <param name="color">Use a color value in string
+    /// <para>example: RTS.Color(Color.blue)</para>
+    /// </param>
+    /// <returns></returns>
     public static Style.S_Color Color(Color color)
     {
         return new Style.S_Color() { value = ColorUtility.ToHtmlStringRGBA(color) };
     }
 
-    /**********************************/
-    /*   RTS ==> "Rich Text String"   */
-    /**********************************/
+    #endregion
+
+    //****** RTS String Construction *******//
+
+    #region RTS String Construction
+
+    /// <summary>
+    /// String with multiples sytles params
+    /// </summary>
+    /// <param name="text">The string you want to format</param>
+    /// <param name="styles">A style array including: RTS.Bold RTS.Italic RTS.Size RTS.Color</param>
+    /// <returns></returns>
     public static string String( string text, params Style[] styles )
     {
         StringBuilder sb = new StringBuilder(text);
@@ -83,35 +139,5 @@ class RTS
         return sb.ToString();
     }
 
-    public static string String(string text, string hexColorWithSharp, bool isBold = false, bool isItalic = false)
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendFormat("<color={0}>{1}</color>", hexColorWithSharp, text);
-
-        if (isBold)
-        {
-            sb.Insert(0, "<b>");
-            sb.Append("</b>");
-        }
-
-        if (isItalic)
-        {
-            sb.Insert(0, "<i>");
-            sb.Append("</i>");
-        }
-
-        return sb.ToString();
-    }
-    public static string String(string text, Color textColor, bool isBold = false, bool isItalic = false)
-    {
-        return String(text, ColorUtility.ToHtmlStringRGBA(textColor), isBold, isItalic);
-
-    }
-
-    //  Exemple of usage : 
-
-    // Add : using static RTS;
-
-    //  Debug.Log(RTString("The Sky is Blue !!!! ", Color.blue, true, false));
-    //  Debug.Log(RTString("The Sky is Blue ! ", Color.blue, true, false)+" "+ RTS.RTString("And Love Is Pink !!!! ", "#E43CFF", false, true));
+    #endregion
 }
